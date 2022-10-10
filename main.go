e511e6661bf4aa4ebab6cb13fdadad3b82b4617d
Stephanie Goldstein <stephanieg0@github.com>
2022-10-10 11:47:59 -0500

    added package dependency

--- main.go
+++ main.go
@@ -4,8 +4,14 @@ import (
 	"fmt"
 
 	"rsc.io/quote/v4"
+
+	"github.com/stephanieg0/hello-go/greetings"
 )
 
 func main() {
 	fmt.Println(quote.Hello())
+
+	message := greetings.Hello("Stephanie")
+
+	fmt.Println(message)
 }
